9de39ea35b0037dec07b9a24b0df82c502f0920c
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2020-10-02 15:29:11 +0200

    fix package

--- R/hello_world.R
+++ R/hello_world.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,15 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+
+#' @title Say Hello
+#'
+#' @description Creates a string with hello word
+#'
+#' @param name A string
+#' @return a string
+
+hello_world <- function(name) {
+  res <- "hello "+name
+  return(res)
 }
