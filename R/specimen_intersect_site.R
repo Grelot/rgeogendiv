6bffab0a4d280ae89bdb33f7869fa8bd653979c0
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2020-10-06 11:08:38 +0200

    add specimen intersect site function

---
+++ R/specimen_intersect_site.R
@@ -0,0 +1,25 @@
+library(sp)
+library(rgeos)
+
+
+
+#' @export
+#' @title specimen spatial points within spatial polygons site of grid
+#' @param specimen.df a data.frame of specimen
+#' @param grid.sp a spatialpolygons object of all sites of the grid
+#' @param projectionCRS Interface class to the PROJ projection and transformation system.
+#' Default value is the projection of the `grid.sp` parameter.
+#' @return a matrix of presence/absence TRUE/FALSE of a specimen within a site
+#' with column as site ID
+#' and row as specimen spatial points ID
+specimen_intersect_site <- function (specimen.df,
+                                  grid.sp,
+                                  projectionCRS= CRS(proj4string(grid.sp))@projargs[1]) {
+  grid.sp.proj <- sp::spTransform(grid.sp,
+                            sp::CRS(projectionCRS))
+  specimen.sp.proj <- spatialpoints_bold_df(specimen.df,
+                                       projectionCRS=projectionCRS)
+  specimenIntersectSites <- rgeos::gIntersects(grid.sp.proj , specimen.sp.proj, byid = TRUE)
+  return(specimenIntersectSites)
+
+}
